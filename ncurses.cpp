49120c08bedbb0ca97883c3e5331ea7808e0c270
James Cline <james.cline@gatech.edu>
2010-07-01 23:23:12 -0400

    We can now create and exit ncurses-shtuff. 'q' exits.

--- ncurses.cpp
+++ ncurses.cpp
@@ -2,7 +2,34 @@
 #include "ncurses.h"
 #include "Show.h"
 
+bool input();
+
 int main( int argc, char **argv) {
 
+	initscr();
+	cbreak();
+	noecho();
+	keypad( stdscr, TRUE );
+	curs_set(0);
+
+	while(input());
+
+
+	endwin();
+
 	return 0;
+}
+
+
+
+bool input() {
+	unsigned int in = getch();
+
+	switch (in) {
+		case 'q':
+			return 0;
+			break;
+	}
+
+	return 1;
 }
