e55fc59cac55d66391cd688c3c4ab50db3a3cd98
James Cline <james.cline@gatech.edu>
2011-07-18 18:36:53 -0400

    Multiple window support
    	- window structure
    		- window names
    		- compact code
    	- setlocale
    	- no passing current WINDOW*

--- ncurses.cpp
+++ ncurses.cpp
@@ -10,15 +10,25 @@
 using std::string;
 using std::vector;
 
-void draw(WINDOW* win);
-bool input(WINDOW* win);
-string& strinput(WINDOW* win);
+void draw();
+void drawwin();
+bool input();
+string& strinput();
 
-WINDOW* playlistwin;
-Playlist playlist;
+WINDOW* dispwin;
+struct wind{
+	WINDOW* win;
+	string name;
+};
+
+vector<wind> windows;
+
+Playlist* playlist;
 
 
 int main( int argc, char **argv) {
+
+	setlocale(LC_ALL, "");
 
 	initscr();
 	cbreak();
@@ -26,28 +36,65 @@ int main( int argc, char **argv) {
 	keypad( stdscr, TRUE );
 	curs_set(0);
 
-	playlistwin = newwin(0,0,1,0);
-	keypad(playlistwin, TRUE);
+	{ // We don't want to keep around names any longer than necessary
+		string names[] = {"Playlist", "Shows", "Edit"}; 
+		windows.reserve(sizeof(names)/sizeof(string)); // Allocate space in vector
+		for(string s: names) {
+			wind n;
+			n.win = newwin(0,0,1,0);
+			n.name = s;
+			windows.push_back(n);
+			keypad(windows.back().win, TRUE);
+		}
+	}
+	dispwin = windows.front().win;
+	playlist = new Playlist();
+
 
 	//TODO: deal with conf loading later
 
-	while(input(playlistwin));
+	draw();
+	while(input());
 
 	endwin();
 
 	return 0;
 }
 
-bool input(WINDOW* win) {
+/*
+	Draws stdscr.
+*/
+void draw() {
+	move(0,0);
+	for(wind &i: windows) {
+		if(i.win == dispwin)
+			printw("%s ", i.name.c_str());
+		else
+			printw("%s ", i.name.c_str());
+	}
+}
+
+/*
+	Draws a given window.
+*/
+void drawwin() {
+	mvwprintw(dispwin,0,0,"%s",playlist->print().c_str());
+	wrefresh(dispwin);
+}
+
+/*
+	Control inputs (up/down/add, etc).
+*/
+bool input() {
 	int in = getch();
 	string s;
 
 	switch (in) {
 		case KEY_ENTER:
 		case '\n':
-			s = strinput(win);
+			s = strinput();
 			if(s.length() > 0)
-				playlist.add(Show(s));
+				playlist->add(Show(s));
 			break;
 		case 'q':
 			return 0;
@@ -56,25 +103,28 @@ bool input(WINDOW* win) {
 			break;
 	}
 
-	draw(win);
+	drawwin();
 	return 1;
 }
 
-string& strinput(WINDOW* win) {
+/*
+	Gets string input, location is the bottom of the given window.
+*/
+string& strinput() {
 	nocbreak();
 	echo();
 	curs_set(1);
 
 	int row,col;
-	getmaxyx(win,row,col);
+	getmaxyx(dispwin,row,col);
 
 	char cstr[col];
 	for(int i = 0; i < col; ++i) cstr[i] = 0;
 
-	mvwgetnstr(win,row-1,0,cstr,col-1);
+	mvwgetnstr(dispwin,row-1,0,cstr,col-1);
 
-	wmove(win,row-1,0);
-	wclrtoeol(win);
+	wmove(dispwin,row-1,0);
+	wclrtoeol(dispwin);
 
 	string* str = new string(cstr);
 
@@ -84,7 +134,3 @@ string& strinput(WINDOW* win) {
 	return *str;
 }
 
-void draw(WINDOW* win) {
-	mvwprintw(win,0,0,"%s",playlist.print().c_str());
-	wrefresh(win);
-}
