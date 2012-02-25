b77923d68705aea686ad086d985b341cec461da6
James Cline <james.cline@gatech.edu>
2012-02-25 14:34:25 -0500

    lolgcc

--- PlaylistWindow.cpp
+++ PlaylistWindow.cpp
@@ -150,7 +150,7 @@ void PlaylistWindow::drawit() {
 void PlaylistWindow::drawheaders(unsigned int cols) {
 	mvwhline ( window, 0, 0, ACS_HLINE, cols);
 	mvwprintw( window, 1, 0, "#\tName");
-	mvwprintw( window, 1, cols - strlen("Watched"), "Watched");
+	mvwprintw( window, 1, cols - std::strlen("Watched"), "Watched");
 	mvwhline ( window, 2, 0, ACS_HLINE, cols);
 }
 
