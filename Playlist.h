a08308f56b912d7108f1ffcde1352f4c5b24d59c
James Cline <james.cline@gatech.edu>
2012-02-25 14:05:58 -0500

    comment format change

--- Playlist.h
+++ Playlist.h
@@ -19,55 +19,93 @@ class Playlist {
 
 		Playlist();
 		Playlist(const std::string& n);
-		// Only use this constructor when initializing from the database
+		/**
+		 * Only use this constructor when initializing from the database
+		 */
 		Playlist(const std::string& n, const Playlist& b);
 		Playlist(const std::string& n, Show& s);
 
 		~Playlist() {}
 
-		// Add a new show to the playlist
+		/**
+		 * Add a new show to the playlist
+		 */
 		void add(Show& s);
-		// Add a new show to the playlist at a given position
+		/**
+		 * Add a new show to the playlist at a given position
+		 */
 		void add(Show& s, size_t pos);
 
-		// Get the iterator to the first element in the playlist
+		/**
+		 * Get the iterator to the first element in the playlist
+		 */
 		auto begin() -> decltype(items.end());
-		// Get the iterator to the last element in the playlist + 1
+		/**
+		 * Get the iterator to the last element in the playlist + 1
+		 */
 		auto end() -> decltype(items.end());
-		// Get the first unplayed element in the playlist for this session
-		// If there is none, returns begin()
+		/**
+		 * Get the first unplayed element in the playlist for this session
+		 * If there is none, returns begin()
+		 */
 		auto first() -> decltype(items.end());
 
 		//! Operator for sorting
 		bool operator< (const Playlist& o) const;
 
-		// Marks this playlist as modified, for methods that can be more efficient with this knowledge
+		/**
+		 * Marks this playlist as modified, for methods that can be more efficient
+		 * with this knowledge
+		 */
 		void change();
-		// Remove the selected item from the playlist
+		/**
+		 * Remove the selected item from the playlist
+		 */
 		void deleteselection(decltype(items.begin()) s);
-		// Get the number of times played?
+		/**
+		 * Get the number of times played?
+		 */
 		unsigned int played();
-		// Remove an item at a given position from the playlist
+		/**
+		 * Remove an item at a given position from the playlist
+		 */
 		void remove(size_t pos);
 
-		// Print the playlist for output of some kind
+		/**
+		 * Print the playlist for output of some kind
+		 */
 		std::string print();
-		// Print a more detailed version of the playlist to a given stream (mainly for saving to a file)
+		/**
+		 * Print a more detailed version of the playlist to a given stream
+		 * (mainly for saving to a file)
+		 */
 		void printdetail(std::ostream& o);
-		// Print an element at a given position in the playlist
+		/**
+		 * Print an element at a given position in the playlist
+		 */
 		std::string print(size_t pos);
 
-		// Return a const reference to the name
+		/**
+		 * Return a const reference to the name
+		 */
 		const std::string& getname() const;
-		// If the name is changed and we need to update it in the database
+		/**
+		 * If the name is changed and we need to update it in the database
+		 */
 		const std::string& getoldname() const;
-		// Return the size of the playlist
+		/**
+		 * Return the size of the playlist
+		 */
 		const size_t size();
 
-		// Return if the playlist data has changed
+		/**
+		 * Return if the playlist data has changed
+		 */
 		bool haschanged();
 
-		// Database type conversion for named identifiers, needs private access
+		/**
+		 * Database type conversion for named identifiers, needs private access
+		 */
 		template<class Playlist> friend struct soci::type_conversion;
 };
 
